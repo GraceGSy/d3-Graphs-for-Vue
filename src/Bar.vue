763f09be42e923a90cb51f4ae7c8f51e97c6ba78
gracegsy <graceguo43@gmail.com>
2018-08-23 22:31:06 +0800

    feat: bars

---
+++ src/Bar.vue
@@ -0,0 +1,42 @@
+<template>
+	<rect :width="width" :height="height" :x="xOffset" :y="yOffset"></rect>
+</template>
+
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+	props: {dimensions: {type: Array, default: () => []}},
+
+	data () {
+		return {
+		};
+	},
+	methods: {
+		scale: function(n) {
+			return n*15
+		}
+	},
+	computed: {
+		width: function() {
+			return this.dimensions[0];
+		},
+		height: function() {
+			return this.dimensions[1];
+		},
+		xOffset: function() {
+			return this.dimensions[2];
+		},
+		yOffset: function() {
+			return this.dimensions[3];
+		},
+	},
+}
+</script>
+
+
+
+<style>
+
+</style>
